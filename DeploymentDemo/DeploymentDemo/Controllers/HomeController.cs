e0807463643e12897238cd9457dcd855fe5e1a46
Muhammad Asif <asif4347@icloud.com>
2016-07-31 05:13:53 -0700

    Modified
    FeedBack Added

--- DeploymentDemo/DeploymentDemo/Controllers/HomeController.cs
+++ DeploymentDemo/DeploymentDemo/Controllers/HomeController.cs
@@ -17,5 +17,9 @@ namespace DeploymentDemo.Controllers
         {
             return View();
         }
+        public ActionResult FeedBack()
+        {
+            return View();
+        }
     }
 }
